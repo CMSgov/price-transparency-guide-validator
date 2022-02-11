d9a8cd30ff6a86cab725376dbb3fca2ac9362db2
mint thompson <mathompson@mitre.org>
2022-02-11 16:35:52 -0500

    Schema version is required argument
    When calling the script, the two required arguments are the path
    to the data file and the schema version. If the specified version
    is not found, the available versions are listed.
    
    Remove option for using the docker container. Always use the docker
    container. Remove function for running validator executable directly.

--- src/index.ts
+++ src/index.ts
@@ -2,46 +2,32 @@
 
 import util from 'util';
 import path from 'path';
-import { execFile, exec } from 'child_process';
+import { exec } from 'child_process';
 import { program } from 'commander';
 import NodeGit from 'nodegit';
 import fs from 'fs-extra';
 import temp from 'temp';
 
 const SCHEMA_REPO_URL = 'https://github.com/CMSgov/price-transparency-guide.git';
-const SCHEMA_ZIP_URL =
-  'https://github.com/CMSgov/price-transparency-guide/archive/refs/heads/master.zip';
 const SCHEMA_REPO_FOLDER = path.normalize(path.join(__dirname, '..', 'schema-repo'));
-
-temp.track();
 
 program
   .name('mr-validator')
-  .usage('<schema-file> <data-file> [options]')
-  .argument('<schema-file>', 'path to schema file')
+  .usage('data-file> <schema-version> [options]')
   .argument('<data-file>', 'path to data file to validate')
-  .option('-s, --schema-version <version>', 'version of schema to use for validation')
+  .argument('<schema-version>', 'version of schema to use for validation')
   .option('-o, --out <out>', 'output path')
-  .option('-d, --docker', 'try to call docker image', false)
   .parse(process.argv);
 
-const options = program.opts();
-const validatorArgs = program.args;
-if (options.out) {
-  validatorArgs.push(options.out);
-}
+temp.track();
 
-if (options.schemaVersion) {
-  useRepoVersion(options.schemaVersion).then(schemaFilePath => {
-    if (schemaFilePath != null) {
-      runContainer(schemaFilePath, validatorArgs[1]);
-    }
-  });
-} else if (options.docker) {
-  runContainer(validatorArgs[0], validatorArgs[1]);
-} else {
-  runValidator(validatorArgs);
-}
+useRepoVersion(program.args[1]).then(schemaPath => {
+  if (schemaPath != null) {
+    runContainer(schemaPath, program.args[0]);
+  } else {
+    console.log('No schema available - not validating.');
+  }
+});
 
 async function runContainer(schemaPath: string, dataPath: string) {
   // figure out mount for schema file
@@ -52,44 +38,25 @@ async function runContainer(schemaPath: string, dataPath: string) {
   const absoluteDataPath = path.resolve(dataPath);
   const dataDir = path.dirname(absoluteDataPath);
   const dataFile = path.basename(absoluteDataPath);
-  const containerId = 'ed20b5a3376d';
-  // incredibly unsafe but temporarily useful, do not merge to main
-  return util
-    .promisify(exec)(
-      `docker run -v ${schemaDir}:/schema/ -v ${dataDir}:/data/ ${containerId} schema/${schemaFile} data/${dataFile}`
-    )
-    .then(result => {
-      console.log('regular result');
-      console.log(result.stdout);
-    })
-    .catch(reason => {
-      console.log(reason.stdout);
-      console.log(reason.stderr);
-    });
-}
-
-async function runValidator(validatorArgs: string[]) {
-  return util
-    .promisify(execFile)(path.join(__dirname, '..', 'validator'), validatorArgs, {
-      cwd: path.join(__dirname, '..'),
-      shell: false
-    })
-    .then(result => {
-      console.log('validation success!');
-      if (!options.out) {
+  const newContainerId = await util
+    .promisify(exec)('docker images validator:latest --format "{{.ID}}"')
+    .then(result => result.stdout.trim());
+  if (newContainerId.length > 0) {
+    // incredibly unsafe but temporarily useful, do not merge to main
+    return util
+      .promisify(exec)(
+        `docker run -v ${schemaDir}:/schema/ -v ${dataDir}:/data/ ${newContainerId} schema/${schemaFile} data/${dataFile}`
+      )
+      .then(result => {
         console.log(result.stdout);
-      }
-      console.log('thank you for using mr-validator!');
-    })
-    .catch(reason => {
-      console.log('validation failure. validation mesage follows:');
-      if (options.out) {
-        console.log('see output file for details.');
-      } else {
+      })
+      .catch(reason => {
         console.log(reason.stdout);
         console.log(reason.stderr);
-      }
-    });
+      });
+  } else {
+    console.log('Could not find a validator docker container.');
+  }
 }
 
 async function useRepoVersion(schemaVersion: string) {
@@ -116,7 +83,11 @@ async function useRepoVersion(schemaVersion: string) {
       return schemaFilePath;
     } else {
       // we didn't find your tag. maybe you mistyped it, so show the available ones.
-      // TODO once there is an update command, remind the user of it
+      console.log(
+        `Could not find a schema version named "${schemaVersion}". Available versions are:\n${tags.join(
+          '\n'
+        )}`
+      );
     }
   } catch (badtime) {
     console.log(badtime);
