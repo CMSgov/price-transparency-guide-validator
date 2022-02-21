16980d643540435cc5a8d7cde1c284a1700c5c0f
mint thompson <mathompson@mitre.org>
2022-02-21 09:00:10 -0500

    Add option to select schema name
    The CMS schema repository provides several different named schemas
    with each versioned released. Add an option that allows the user
    to specify which schema to use for validation.
    
    Refactor functions in new utils file to facilitate testing. Add
    dependencies and configuration for testing. No tests are added yet.

--- src/index.ts
+++ src/index.ts
@@ -3,13 +3,10 @@
 import util from 'util';
 import path from 'path';
 import { exec } from 'child_process';
-import { program, OptionValues } from 'commander';
-import NodeGit from 'nodegit';
+import { program, OptionValues, Option } from 'commander';
 import fs from 'fs-extra';
-import temp from 'temp';
 
-const SCHEMA_REPO_URL = 'https://github.com/CMSgov/price-transparency-guide.git';
-const SCHEMA_REPO_FOLDER = path.normalize(path.join(__dirname, '..', 'schema-repo'));
+import { config, runContainer, useRepoVersion } from './utils';
 
 main().catch(error => {
   console.log(`Encountered an unexpected error: ${error}`);
@@ -19,12 +16,17 @@ async function main() {
   program
     .name('mr-validator')
     .description('Tool for validating health coverage machine-readable files.')
-    .command('validate', { isDefault: true })
-    .description('Validate a file against a specific published version of the schema.')
+    .command('validate')
+    .description('Validate a file against a specific published version of a CMS schema.')
     .usage('<data-file> <schema-version> [options]')
     .argument('<data-file>', 'path to data file to validate')
     .argument('<schema-version>', 'version of schema to use for validation')
     .option('-o, --out <out>', 'output path')
+    .addOption(
+      new Option('-t, --target <schema>', 'name of schema to use')
+        .choices(config.AVAILABLE_SCHEMAS)
+        .default('in-network-rates')
+    )
     .action(validate);
 
   program
@@ -36,9 +38,8 @@ async function main() {
 }
 
 async function validate(dataFile: string, schemaVersion: string, options: OptionValues) {
-  temp.track();
-
-  useRepoVersion(schemaVersion).then(schemaPath => {
+  // get the schema that matches the chosen version and target name. then, use it to validate.
+  useRepoVersion(schemaVersion, options.target).then(schemaPath => {
     if (schemaPath != null) {
       runContainer(schemaPath, dataFile, options.out);
     } else {
@@ -50,97 +51,16 @@ async function validate(dataFile: string, schemaVersion: string, options: Option
 async function update() {
   try {
     // check if the repo exists. if not, clone it. if it exists, fetch updates.
-    if (!fs.existsSync(path.join(SCHEMA_REPO_FOLDER, '.git'))) {
-      await util.promisify(exec)(`git clone ${SCHEMA_REPO_URL} "${SCHEMA_REPO_FOLDER}"`);
+    if (!fs.existsSync(path.join(config.SCHEMA_REPO_FOLDER, '.git'))) {
+      await util.promisify(exec)(
+        `git clone ${config.SCHEMA_REPO_URL} "${config.SCHEMA_REPO_FOLDER}"`
+      );
       console.log('Retrieved schemas.');
     } else {
-      await util.promisify(exec)(`git -C "${SCHEMA_REPO_FOLDER}" pull --no-rebase -t`);
+      await util.promisify(exec)(`git -C "${config.SCHEMA_REPO_FOLDER}" pull --no-rebase -t`);
       console.log('Updated schemas.');
     }
   } catch (error) {
     console.log(`Error when updating available schemas: ${error}`);
-  }
-}
-
-async function runContainer(schemaPath: string, dataPath: string, outputPath: string) {
-  try {
-    // figure out mount for schema file
-    const absoluteSchemaPath = path.resolve(schemaPath);
-    const schemaDir = path.dirname(absoluteSchemaPath);
-    const schemaFile = path.basename(absoluteSchemaPath);
-    // figure out mount for data file
-    const absoluteDataPath = path.resolve(dataPath);
-    const dataDir = path.dirname(absoluteDataPath);
-    const dataFile = path.basename(absoluteDataPath);
-    // figure out mount for output file, if provided
-    let outputDir, outputFile;
-    if (outputPath?.length > 0) {
-      const absoluteOutputPath = path.resolve(outputPath);
-      outputDir = path.dirname(absoluteOutputPath);
-      outputFile = path.basename(absoluteOutputPath);
-    }
-    const newContainerId = await util
-      .promisify(exec)('docker images validator:latest --format "{{.ID}}"')
-      .then(result => result.stdout.trim())
-      .catch(reason => {
-        console.log(reason.stderr);
-        return '';
-      });
-    if (newContainerId.length > 0) {
-      let runCommand: string;
-      if (outputDir && outputFile) {
-        runCommand = `docker run -v "${schemaDir}":/schema/ -v "${dataDir}":/data/ -v "${outputDir}":/output/ ${newContainerId} "schema/${schemaFile}" "data/${dataFile}" -o "output/${outputFile}"`;
-      } else {
-        runCommand = `docker run -v "${schemaDir}":/schema/ -v "${dataDir}":/data/ ${newContainerId} "schema/${schemaFile}" "data/${dataFile}"`;
-      }
-      return util
-        .promisify(exec)(runCommand)
-        .then(result => {
-          console.log(result.stdout);
-        })
-        .catch(reason => {
-          console.log(reason.stdout);
-          console.log(reason.stderr);
-        });
-    } else {
-      console.log('Could not find a validator docker container.');
-    }
-  } catch (error) {
-    console.log(`Error when running validator container: ${error}`);
-  }
-}
-
-async function useRepoVersion(schemaVersion: string) {
-  try {
-    // check if the repo exists, and if not, try to clone it
-    if (!fs.existsSync(path.join(SCHEMA_REPO_FOLDER, '.git'))) {
-      await util.promisify(exec)(`git clone ${SCHEMA_REPO_URL} "${SCHEMA_REPO_FOLDER}"`);
-    }
-    const repo = await NodeGit.Repository.open(SCHEMA_REPO_FOLDER);
-    const tags = await NodeGit.Tag.list(repo);
-    if (tags.includes(schemaVersion)) {
-      // if the specified version is a valid tag, get the schema file at that tag's commit
-      const versionTag = await repo.getTagByName(schemaVersion);
-      const versionTarget = await versionTag.target();
-      const versionCommit = await repo.getCommit(versionTarget.id());
-      const versionEntry = await versionCommit.getEntry(
-        path.join('schemas', 'in-network-rates', 'in-network-rates.json')
-      );
-      const blob = await versionEntry.getBlob();
-      // write this version of the schema to a temporary file
-      const schemaDir = temp.mkdirSync('schemas');
-      const schemaFilePath = path.join(schemaDir, 'schema.json');
-      fs.writeFileSync(schemaFilePath, blob.content());
-      return schemaFilePath;
-    } else {
-      // we didn't find your tag. maybe you mistyped it, so show the available ones.
-      console.log(
-        `Could not find a schema version named "${schemaVersion}". Available versions are:\n${tags.join(
-          '\n'
-        )}`
-      );
-    }
-  } catch (error) {
-    console.log(`Error when accessing schema: ${error}`);
   }
 }
---
+++ src/utils.ts
@@ -0,0 +1,113 @@
+import util from 'util';
+import path from 'path';
+import { exec } from 'child_process';
+import NodeGit from 'nodegit';
+import fs from 'fs-extra';
+import temp from 'temp';
+
+export const config = {
+  AVAILABLE_SCHEMAS: [
+    'allowed-amounts',
+    'in-network-rates',
+    'provider-reference',
+    'table-of-contents'
+  ],
+  SCHEMA_REPO_URL: 'https://github.com/CMSgov/price-transparency-guide.git',
+  SCHEMA_REPO_FOLDER: path.normalize(path.join(__dirname, '..', 'schema-repo'))
+};
+
+export async function ensureRepo(repoDirectory: string) {
+  // check if the repo exists, and if not, try to clone it
+  if (!fs.existsSync(path.join(repoDirectory, '.git'))) {
+    return util.promisify(exec)(`git clone ${config.SCHEMA_REPO_URL} "${repoDirectory}"`);
+  }
+}
+
+export async function useRepoVersion(schemaVersion: string, schemaName: string) {
+  try {
+    await ensureRepo(config.SCHEMA_REPO_FOLDER);
+    const repo = await NodeGit.Repository.open(config.SCHEMA_REPO_FOLDER);
+    const tags = await NodeGit.Tag.list(repo);
+    if (tags.includes(schemaVersion)) {
+      // if the specified version is a valid tag, get the schema file at that tag's commit
+      const versionTag = await repo.getTagByName(schemaVersion);
+      const versionTarget = await versionTag.target();
+      const versionCommit = await repo.getCommit(versionTarget.id());
+      const versionEntry = await versionCommit.getEntry(
+        path.join('schemas', schemaName, `${schemaName}.json`)
+      );
+      const blob = await versionEntry.getBlob();
+      // write this version of the schema to a temporary file
+      temp.track();
+      const schemaDir = temp.mkdirSync('schemas');
+      const schemaFilePath = path.join(schemaDir, 'schema.json');
+      fs.writeFileSync(schemaFilePath, blob.content());
+      return schemaFilePath;
+    } else {
+      // we didn't find your tag. maybe you mistyped it, so show the available ones.
+      console.log(
+        `Could not find a schema version named "${schemaVersion}". Available versions are:\n${tags.join(
+          '\n'
+        )}`
+      );
+    }
+  } catch (error) {
+    console.log(`Error when accessing schema: ${error}`);
+  }
+}
+
+function buildRunCommand(
+  schemaPath: string,
+  dataPath: string,
+  outputPath: string,
+  containerId: string
+): string {
+  // figure out mount for schema file
+  const absoluteSchemaPath = path.resolve(schemaPath);
+  const schemaDir = path.dirname(absoluteSchemaPath);
+  const schemaFile = path.basename(absoluteSchemaPath);
+  // figure out mount for data file
+  const absoluteDataPath = path.resolve(dataPath);
+  const dataDir = path.dirname(absoluteDataPath);
+  const dataFile = path.basename(absoluteDataPath);
+  // figure out mount for output file, if provided
+  let outputDir, outputFile;
+  if (outputPath?.length > 0) {
+    const absoluteOutputPath = path.resolve(outputPath);
+    outputDir = path.dirname(absoluteOutputPath);
+    outputFile = path.basename(absoluteOutputPath);
+  }
+  if (outputDir && outputFile) {
+    return `docker run -v "${schemaDir}":/schema/ -v "${dataDir}":/data/ -v "${outputDir}":/output/ ${containerId} "schema/${schemaFile}" "data/${dataFile}" -o "output/${outputFile}"`;
+  } else {
+    return `docker run -v "${schemaDir}":/schema/ -v "${dataDir}":/data/ ${containerId} "schema/${schemaFile}" "data/${dataFile}"`;
+  }
+}
+
+export async function runContainer(schemaPath: string, dataPath: string, outputPath: string) {
+  try {
+    const containerId = await util
+      .promisify(exec)('docker images validator:latest --format "{{.ID}}"')
+      .then(result => result.stdout.trim())
+      .catch(reason => {
+        console.log(reason.stderr);
+        return '';
+      });
+    if (containerId.length > 0) {
+      const runCommand = buildRunCommand(schemaPath, dataPath, outputPath, containerId);
+      return util
+        .promisify(exec)(runCommand)
+        .then(result => {
+          console.log(result.stdout);
+        })
+        .catch(reason => {
+          console.log(reason.stdout);
+          console.log(reason.stderr);
+        });
+    } else {
+      console.log('Could not find a validator docker container.');
+    }
+  } catch (error) {
+    console.log(`Error when running validator container: ${error}`);
+  }
+}
