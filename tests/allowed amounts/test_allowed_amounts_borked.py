d9b43ed004d3f4c0eb2420b5c8f022b465d05538
carrils <scarrillo578@gmail.com>
2022-03-07 15:26:38 -0600

    bring branch up with most recent changed to forked repo

--- tests/allowed amounts/test_allowed_amounts_borked.py
+++ tests/allowed amounts/test_allowed_amounts_borked.py
@@ -1,14 +1,24 @@
-# Test data-files/allowed-amounts-borked.json against its schema 
+# Test data-files/allowed-amounts-borked.json against its schema
 import subprocess
 
-# check for exceptions
-def test_schema_raises_no_exception():
-    try:
-        schema_test_outcome = subprocess.call(["/home/runner/work/price-transparency-guide-validator/price-transparency-guide-validator/validator", "/home/runner/work/price-transparency-guide-validator/price-transparency-guide-validator/schemas/allowed-amounts.json", "/home/runner/work/price-transparency-guide-validator/price-transparency-guide-validator/data-files/allowed-amounts-borked.json"])
-    except Exception as exc:
-        assert False, f"'allowed-amounts-borked.json' raised an exception {exc}"
+# #check for exceptions thrown by a run of the validator
+# def test_allowed_amounts_borked_raises_no_exception():
+#     try:
+#         run = subprocess.Popen(
+#             (["./validator", "../../schemas/allowed-amounts.json",
+#              "../../data-files/allowed-amounts-borked.json"]),
+#             stdout=subprocess.PIPE,
+#             stderr=subprocess.PIPE,
+#             text=True
+#         )
+#     except Exception as exc:
+#         assert False, f"'allowed-amounts-borked.json' raised an exception {exc}"
 
-# test to make sure this one failed the validator. 1 == failure.
+
+# make sure borked json file fails validation
+# 0 == successful, 1 == failure
 def test_allowed_amounts_borked():
-    schema_test_outcome = subprocess.call(["/home/runner/work/price-transparency-guide-validator/price-transparency-guide-validator/validator", "/home/runner/work/price-transparency-guide-validator/price-transparency-guide-validator/schemas/allowed-amounts.json", "/home/runner/work/price-transparency-guide-validator/price-transparency-guide-validator/data-files/allowed-amounts-borked.json"])
-    assert schema_test_outcome == 1
+    cmd = ["sudo", "../../validator", "../../schemas/allowed-amounts.json", "../../data-files/allowed-amounts-borked.json"]
+    run = subprocess.Popen(cmd, shell=True, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    run.communicate()
+    assert run.returncode == 1
--- tests/allowed amounts/test_allowed_amounts_sample.py
+++ tests/allowed amounts/test_allowed_amounts_sample.py
@@ -1,14 +1,29 @@
 # Test /data-files/allowed-amounts.json against the allowed amounts schema.
+from os import chmod
+import stat
 import subprocess
 
-# check for exceptions thrown by the file itself
-def test_schema_raises_no_exception():
-    try:
-        schema_test_outcome = subprocess.call(["/home/runner/work/price-transparency-guide-validator/price-transparency-guide-validator/validator", "/home/runner/work/price-transparency-guide-validator/price-transparency-guide-validator/schemas/allowed-amounts/allowed-amounts.json", "/home/runner/work/price-transparency-guide-validator/price-transparency-guide-validator/data-files/allowed-amounts.json"])
-    except Exception as exc:
-        assert False, f"'allowed-amounts.json' raised an exception {exc}"
+# # check for exceptions thrown by a run of the validator
+# def test_allowed_amounts_raises_no_exception():
+#     try:
+#         run = subprocess.Popen(
+#             (["./validator", "../../schemas/allowed-amounts.json",
+#              "../../data-files/allowed-amounts.json"]),
+#             stdout=subprocess.PIPE,
+#             stderr=subprocess.PIPE,
+#             text=True
+#         )
+#     except Exception as exc:
+#         assert False, f"'allowed-amounts.json' raised an exception {exc}"
 
-# check if it validates successfully. 0 == success. 
+
+# check to see if file passes validation
 def test_allowed_amounts():
-    schema_test_outcome = subprocess.call(["/home/runner/work/price-transparency-guide-validator/price-transparency-guide-validator/validator", "/home/runner/work/price-transparency-guide-validator/price-transparency-guide-validator/schemas/allowed-amounts/allowed-amounts.json", "/home/runner/work/price-transparency-guide-validator/price-transparency-guide-validator/data-files/allowed-amounts.json"])
-    assert schema_test_outcome == 0
+    cmd = ["sudo", "../../validator", "../../schemas/allowed-amounts.json", "../../data-files/allowed-amounts.json"]
+    run = subprocess.Popen(cmd, text=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)  # only set shell=True when cmd is one arg not a list.
+    output, error= run.communicate()
+    print("input: ", run.stdin)
+    print("output: ", output)
+    print("stderr: ", error)
+    print("run: ", run)
+    assert output == "Input JSON is valid.\n"
